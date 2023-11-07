443979ba03df9300873ccb45890dedb068bf970e
serichannu <148667375+serichannu@users.noreply.github.com>
2023-11-07 09:29:56 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,2 +1,2 @@
 var today =new Date(2023, 11, 6);
-console.log(today.getFullYear() + "年" + today.getMonth() + "月" + today.getDate() + "日");
+console.log(today.getFullYear() + "年" + today.getMonth(+1) + "月" + today.getDate() + "日");
